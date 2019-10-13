796d4cb6fd12d8c72c114fad0dc7c6fa2f64a1be
Scott Matsuda <smatsuda2014@gmail.com>
2019-10-13 07:28:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array, string)
-  array.insert(string, 4)
+def using_insert(array, element)
+  array.insert(4, element)
 end
 
 def using_uniq(array)
